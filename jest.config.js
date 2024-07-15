684b1719c8780bcfcba52ed5159135e72c7ca543
luscabap <lucasbaptistasilva.dev@gmail.com>
2024-07-15 18:05:49 -0300

    :tada: Base project

---
+++ jest.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  testEnvironment: 'jest-environment-jsdom',
+  setupFilesAfterEnv: ['<rootDir>/src/setupTests.ts'],
+  moduleNameMapper: {
+    '\\.(git|ttf|eot|svg|png)$': '<rootDir>/.jest/mock/fileMock.js',
+    '\\.(css|less|sass|scss)$': 'identity-obj-proxy',
+  },
+  transform: {
+    '^.+\\.(ts|tsx)$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+}
---
+++ src/App.tsx
@@ -0,0 +1,5 @@
+function App() {
+  return ( <h1 className="bg-primaryColor">Mauz</h1> )
+}
+
+export default App
