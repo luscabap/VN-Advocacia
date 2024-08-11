1e4a2464779423febd0ebfe6eed873d3367ebdac
luscabap <lucasbaptistasilva.dev@gmail.com>
2024-08-11 19:05:27 -0300

    fixing responsiviness

--- src/Components/ContactSocialMedia/index.tsx
+++ src/Components/ContactSocialMedia/index.tsx
@@ -9,16 +9,16 @@ interface IContactProps {
 export const ContactSocialMedia = ({ iconSize, iconColor }: IContactProps) => {
   return (
     <div className="flex justify-evenly items-center">
-        <a href="https://www.instagram.com/vnadvocacia_/" target="_blank">
+        <a href="https://www.instagram.com/vnadvocacia_/" target="_blank" className=" w-32 flex justify-center py-1 rounded-md">
           <FaInstagram size={iconSize}  color={iconColor}/>
         </a>
         <a href="https://wa.me/558681688351?text=Olá,+gostaria+de+informações+sobre+os+seus+serviços,+por+favor." target="_blank" className=" w-32 flex justify-center py-1 rounded-md">
           <FaWhatsapp size={iconSize}  color={iconColor}/>
         </a>
-        <a href="https://linktr.ee/vnadvogados?utm_source=linktree_admin_share" target="_blank">
+        <a href="https://linktr.ee/vnadvogados?utm_source=linktree_admin_share" target="_blank" className=" w-32 flex justify-center py-1 rounded-md">
           <SiLinktree size={iconSize}  color={iconColor}/>
         </a>
-        <a href="https://www.linkedin.com/company/vn-advocacia-e-consultoria-jur%C3%ADdica/posts/?feedView=all" target="_blank">
+        <a href="https://www.linkedin.com/company/vn-advocacia-e-consultoria-jur%C3%ADdica/posts/?feedView=all" target="_blank" className=" w-32 flex justify-center py-1 rounded-md">
           <FaLinkedin size={iconSize}  color={iconColor}/>
         </a>
     </div>
